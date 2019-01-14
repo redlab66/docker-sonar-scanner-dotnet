885ab5651423b3bd77e4808326eaa8e22b4d590f
Yann Ponzoni <yann.ponzoni@redfabriq.com>
2019-01-14 16:15:09 +0100

    Fix typo in generated xml file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set +e
 
 # Write the configuration file from env vars
 echo "
-<SonarQubeAnalysisProperties  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.sonarsource.com/msbuild/integration/2015/1">
-  <Property Name="sonar.host.url">${SONAR_HOST_URL}</Property>
-  <Property Name="sonar.login">${SONAR_LOGIN}</Property>
-  <Property Name="sonar.password">${SONAR_PASSWORD}</Property>
+<SonarQubeAnalysisProperties  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://www.sonarsource.com/msbuild/integration/2015/1\">
+  <Property Name=\"sonar.host.url\">${SONAR_HOST_URL}</Property>
+  <Property Name=\"sonar.login\">${SONAR_LOGIN}</Property>
+  <Property Name=\"sonar.password\">${SONAR_PASSWORD}</Property>
 </SonarQubeAnalysisProperties>
 " > /opt/sonarscanner/SonarQube.Analysis.xml
 
